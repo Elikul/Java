e140380fe43aace60dac8dbe0568d52274f78aac
Elikul <linotska@inbox.ru>
2019-11-22 15:37:59 +0300

    Исправленное домашние задание урок 2 (более правильное)

--- lesson2/MyException/src/MyException.java
+++ lesson2/MyException/src/MyException.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * ДЗ с исключениями
  */
@@ -6,90 +8,54 @@ public class MyException {
     //исполнение
     public static void main(String[] args) {
 
-        String[][] mas = new String[][]{
-                {"1", "2", "3","4"},
-                {"5", "6", "7","8"}
-        };
+        Random random = new Random();
+        String[][] mas = new String[4][4];
+        int result = 0;
 
-        String[][] mas1 = new String[][]{
-                {"1", "ваыуа", "3","4"},
-                {"5", "6", "7","уауа"}
-        };
-
-        String[][] mas2 = new String[][]{
-                {"1", "2", "3","4"},
-                {"5", "6", "7"}
-        };
+        for(int i = 0; i < 4; i++) {
+            for(int j = 0; j < 4; j++) {
+                mas[i][j] = String.valueOf(random.nextInt(50));
+            }
+        }
 
         try {
-            int result = Sum(mas);
-            System.out.println("Сумма равна " + result);
+           result = Sum(mas);
 
-//            int result1 = Sum(mas1);
-//            System.out.println("Сумма равна " + result1);
-
-//            int result2 = Sum(mas2);
-//            System.out.println("Сумма равна " + result2);
         } catch (MyArraySizeException | MyArrayDataException e) {
             e.printStackTrace();
         }
-
+        System.out.println("Сумма равна " + result);
     }
 
     //метод суммы
-    public static int Sum(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+    public static int Sum(String[][] arr){
         int result = 0;
 
-        if((arr.length != 4) && (arr[0].length !=4)){
-            throw new MyArraySizeException("Неправильный размер двумернного массива! Необходим 4*4.",arr.length,arr[0].length);
-        }else  if(arr[1].length !=4){
-            throw new MyArraySizeException("Неправильный размер двумернного массива! Необходим 4*4.",arr.length,arr[1].length);
-        }
-
-
+        if(arr.length != 4) throw new MyArraySizeException();
         for(int i = 0; i < arr.length; i++) {
-            for(int j = 0; j < arr[i].length; j++){
-                   if(!isInteger(arr[i][j]))
-                       throw new MyArrayDataException("Неверные данные в двумернном массиве! Ошибка в ячейке ",i,j);
+            if(arr[i].length != 4) throw new MyArraySizeException();
+            for(int j = 0; j < arr[0].length; j++) {
+                try {
                     result += Integer.parseInt(arr[i][j]);
+                }catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неверные данные в двумернном массиве! Ошибка в ячейке ",i, j);
+                }
             }
         }
         return result;
     }
 
-    //метод: можно ли строку конвертировать в целое число
-    public static boolean isInteger(String str){
-        try {
-            Integer.parseInt(str);
-            return true;
-        }catch (NumberFormatException e){
-            return false;
-        }
-    }
 
 }
 
 /**
  * Исключение неправильный размер двумернного массива
  */
-class MyArraySizeException extends Exception{
-    private int Nrow,Ncol; //количсетво строк, количество столбцов двумернного массива
-
-    //метод получение количества столбцов
-    public int getCol() {
-        return Ncol;
-    }
-
-    //метод получение количества строк
-    public int getRow() {
-        return Nrow;
-    }
+class MyArraySizeException extends RuntimeException{
 
     //конструктор
-    public MyArraySizeException(String msg, int row, int col) {
-        super(msg);
-        this.Nrow = row;
-        this.Ncol = col;
+    public MyArraySizeException() {
+        super("Неправильный размер двумернного массива! Необходим 4*4.");
     }
 }
 
@@ -97,7 +63,7 @@ class MyArraySizeException extends Exception{
 /**
  * Исключение неправильных данных(не чисел) в двумернном массиве
  */
-class MyArrayDataException extends Exception{
+class MyArrayDataException extends RuntimeException{
     private int Nrow,Ncol; //номер ячейки,где лежат неверные данные
 
     //метод получение строки,где лежат неверные данные
